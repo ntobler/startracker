88b5f839e26265657d5b633cb46080efe9b1e5f5
Nicolas Tobler <nitobler@gmail.com>
2023-10-05 23:14:53 +0200

    Added main.py and setup.py

---
+++ startracker/main.py
@@ -0,0 +1,10 @@
+"""Main entrypoint for the startracker application."""
+
+
+def main() -> int:
+    print("Currently i do nothing at all.")
+    return 0
+
+
+if __name__ == "__main__":
+    raise SystemExit(main)
