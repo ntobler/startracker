1a6af598d4e2bbe11233cf7f63f79d053f7a6ff6
Nicolas <nitobler@gmail.com>
2023-11-27 14:30:13 +0100

    debug

--- install_service.sh
+++ install_service.sh
@@ -3,3 +3,7 @@ sudo cp startracker.service /etc/systemd/system
 sudo systemctl daemon-reload
 sudo systemctl enable startracker.service
 sudo systemctl start startracker.service
+
+sudo cp nginx.conf /etc/nginx/sites-available/default
+sudo systemctl enable nginx.service
+sudo systemctl restart nginx.service
