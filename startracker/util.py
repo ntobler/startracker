9cd647c85c9d53a982888d9462950b91851f59c5
Nicolas Tobler <nitobler@gmail.com>
2024-09-11 23:57:10 +1200

    Fixed max_rate

--- startracker/util.py
+++ startracker/util.py
@@ -7,7 +7,7 @@ import time
 @contextlib.contextmanager
 def max_rate(max_rate_hz: float):
     """Pad a block of code with sleep, such that maximum rate is not exceeded."""
-    t = time.monotonic() - 1 / max_rate_hz
+    t = time.monotonic() + 1 / max_rate_hz
     yield
-    if (diff := time.monotonic() - t) > 0:
+    if (diff := t - time.monotonic()) > 0:
         time.sleep(diff)
